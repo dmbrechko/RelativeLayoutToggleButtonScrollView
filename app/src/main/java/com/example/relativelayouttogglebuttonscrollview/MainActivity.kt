08802c1d235cccc318263f621e156da4f5becf75
Dimitrij Brechko <brechkodm@gmail.com>
2024-11-28 13:10:10 +0300

    Commit

---
+++ app/src/main/java/com/example/relativelayouttogglebuttonscrollview/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.relativelayouttogglebuttonscrollview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.relativelayouttogglebuttonscrollview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.apply {
+            loadBTN.setOnClickListener {
+                val text = loadBook(Database().txt).joinToString(" \n")
+                textTV.text = text
+            }
+        }
+    }
+
+    fun loadBook(txt: String): List<String> = txt.split(" ")
+}
